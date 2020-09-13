3fd455bcec740915eed2f9e5393d4bb7e185d260
曲荧 <3468355524@qq.com>
2020-09-13 17:03:17 +0800

    存储过程分页
    存储过程分页

--- BTFsqlproc.sql
+++ BTFsqlproc.sql
@@ -77,3 +77,70 @@ as
 
 
 
+--存储过程分页
+select * from(
+select ROW_NUMBER() over(order by id) as RomNum , * from [Game]
+) as t where t.RomNum between 1 and 5
+USE bgDB
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROC Game_Select
+(
+	@GType_id int ,
+	@Studio_id int,
+	@Game_gState int,
+	@Game_gName nvarchar(20),
+
+	@PageIndex INT,
+	@PageSize INT,
+	@TotalCount INT OUTPUT --定义为输出参数
+)
+AS
+DECLARE @StrSql NVARCHAR(1024);--定义拼接SQL语句变量
+DECLARE @strWhere NVARCHAR(1024)--定义where条件的变量
+DECLARE @PageCount INT --定义总页数
+SET @StrWhere=''
+
+
+ if(@GType_id>0)begin
+		set @strWhere=@strWhere+' and s2.id =  '+cast(@GType_id as nvarchar(10))
+	end
+	if(@Studio_id>0)begin
+		set @strWhere=@strWhere+' and s3.id =  '+cast(@Studio_id as nvarchar(10))
+	end
+	if(@Game_gState >-1)begin
+		set @strWhere=@strWhere+' and s1.gState = '+cast(@Game_gState as nvarchar(10))
+	end
+	if(@Game_gName <>'' and @Game_gName is not null )begin
+		set @strWhere=@strWhere+' and s1.gName like ''%'+@Game_gName+'%'''
+	end
+
+SET @StrSql=' select s3.sName,s2.name,s1.* from Game as s1,GType as s2,Studio as s3 where s1.gTid=s2.id and s1.gSId=s3.id  '+@strWhere
+
+EXEC sp_executesql @StrSql,N'@RowCount INT OUTPUT',@TotalCount OUTPUT  --获取数据总记录数
+
+
+IF (@PageIndex<1)
+BEGIN
+	SET @PageIndex=1;--如果当前页索引小于1，则显示首页数据
+END 
+
+
+SET @PageCount=CEILING(CONVERT(FLOAT,@TotalCount) /@PageSize)--获取总页数
+
+IF(@PageIndex>@PageCount)
+BEGIN
+	SET @PageIndex=@PageCount
+END
+
+SET @StrSql='select * from(
+select ROW_NUMBER() over(order by id) as RomNum , * from [Game] where 1=1'+@strWhere+'
+) as t where t.RomNum between '+CONVERT(NVARCHAR(32),((@PageIndex-1)*@PageSize+1))+' and '+CONVERT(NVARCHAR(32),(@PageIndex*@PageSize)) 
+
+EXEC sp_executesql @StrSql
+
+--'select * from( select ROW_NUMBER() over(order by id) as RomNum , * from [Game] where 1=1'+@strWhere+' ) as t where t.RomNum between '+CONVERT(NVARCHAR(32),((@PageIndex-1)*@PageSize+1))+'  and '+CONVERT(NVARCHAR(32),(@PageIndex*@PageSize)) 
